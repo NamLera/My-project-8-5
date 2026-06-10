57a9dea794f156856914aca16c6c36484c970ac4
Nam Lera <nampan.valeriya@gmail.com>
2026-06-10 19:11:40 +0900

    fix: rename variable showAllBth to showAllBtn in script.js

--- script.js
+++ script.js
@@ -15,15 +15,33 @@ var swiper = new Swiper(".mySwiper", {
         768: {
             slidesPerView: 3,
             spaceBetween: 10,
-            grid: { rows: 4, fill: "row" },
+            grid: { rows: 2, fill: "row" },
         },
 
         1120: {
             slidesPerView: 4,
             spaceBetween: 15,
-            grid: { rows: 3, fill: "row" },
+            grid: { rows: 2, fill: "row" },
         },
     },
+});
+var showAllBtn = document.getElementById("showAll");
+var hideAllBtn = document.getElementById("hideAll");
 
+showAllBtn.addEventListener("click", function() {
+    if (window.innerWidth >= 1120) {
+        swiper.params.grid.rows = 3;
+    } else {
+        swiper.params.grid.rows = 4;
+    }
+    swiper.update();
+    showAllBtn.style.display = "none";
+    hideAllBtn.style.display = "inline-flex";
+});
 
+hideAllBtn.addEventListener("click", function() {
+    swiper.params.grid.rows = 2;
+    swiper.update();
+    hideAllBtn.style.display = "none";
+    showAllBtn.style.display = "inline-flex";
 });
